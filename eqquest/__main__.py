74031797dfc80572bb88a6db2824f73c840d5537
Robert Valentine <paraboliclabs@gmail.com>
2026-08-14 07:33:52 -0400

    Install knowledge coverage UI for module launcher

--- eqquest/__main__.py
+++ eqquest/__main__.py
@@ -1,6 +1,8 @@
 from .runtime_policy import install_runtime_policy
+from .knowledge_coverage_ui import install_knowledge_coverage_ui
 from .runtime import main
 
 if __name__ == "__main__":
     install_runtime_policy()
+    install_knowledge_coverage_ui()
     main()

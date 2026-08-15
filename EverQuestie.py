07d6c779042d7e6405113fbebebdfb6debe08db4
Robert Valentine <paraboliclabs@gmail.com>
2026-08-15 01:10:26 -0400

    Install Knowledge world location UI

--- EverQuestie.py
+++ EverQuestie.py
@@ -3,6 +3,7 @@ from eqquest.runtime_policy import install_runtime_policy
 from eqquest.map_loading_policy import install_map_loading_policy
 from eqquest.knowledge_coverage_ui import install_knowledge_coverage_ui
 from eqquest.packaged_ui_policy import install_packaged_ui_policy
+from eqquest.world_entity_context_ui import install_world_entity_context_ui
 from eqquest.runtime import main
 
 if __name__ == "__main__":
@@ -10,4 +11,5 @@ if __name__ == "__main__":
     install_map_loading_policy()
     install_knowledge_coverage_ui()
     install_packaged_ui_policy()
+    install_world_entity_context_ui()
     main()

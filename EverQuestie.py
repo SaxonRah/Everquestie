7eaa99d0844d58f3dad3b896c8e1bf9de673b14a
Robert Valentine <paraboliclabs@gmail.com>
2026-08-15 22:04:09 -0400

    Install gameplay world profile UI

--- EverQuestie.py
+++ EverQuestie.py
@@ -5,6 +5,7 @@ from eqquest.knowledge_coverage_ui import install_knowledge_coverage_ui
 from eqquest.packaged_ui_policy import install_packaged_ui_policy
 from eqquest.knowledge_relationship_ui import install_knowledge_relationship_navigation_ui
 from eqquest.travel_output_ui import install_travel_output_ui
+from eqquest.world_profile_ui import install_world_profile_ui
 from eqquest.runtime_mode_ui import install_runtime_mode_ui
 from eqquest.runtime import main
 
@@ -15,5 +16,6 @@ if __name__ == "__main__":
     install_packaged_ui_policy()
     install_knowledge_relationship_navigation_ui()
     install_travel_output_ui()
+    install_world_profile_ui()
     install_runtime_mode_ui()
     main()

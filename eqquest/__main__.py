f8f9e898cb330f5006545ab0c59102b55b041bca
Robert Valentine <paraboliclabs@gmail.com>
2026-08-15 21:47:09 -0400

    Keep package launcher policies aligned

--- eqquest/__main__.py
+++ eqquest/__main__.py
@@ -2,6 +2,9 @@ from .runtime_policy import install_runtime_policy
 from .map_loading_policy import install_map_loading_policy
 from .knowledge_coverage_ui import install_knowledge_coverage_ui
 from .packaged_ui_policy import install_packaged_ui_policy
+from .knowledge_relationship_ui import install_knowledge_relationship_navigation_ui
+from .travel_output_ui import install_travel_output_ui
+from .runtime_mode_ui import install_runtime_mode_ui
 from .runtime import main
 
 if __name__ == "__main__":
@@ -9,4 +12,7 @@ if __name__ == "__main__":
     install_map_loading_policy()
     install_knowledge_coverage_ui()
     install_packaged_ui_policy()
+    install_knowledge_relationship_navigation_ui()
+    install_travel_output_ui()
+    install_runtime_mode_ui()
     main()

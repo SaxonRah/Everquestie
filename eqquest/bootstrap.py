d7df19e005e5d72e9ec35f70f6d8fa9632649496
Robert Valentine <paraboliclabs@gmail.com>
2026-08-16 21:04:02 -0400

    Install manual tracked-step completion layer

--- eqquest/bootstrap.py
+++ eqquest/bootstrap.py
@@ -13,6 +13,7 @@ from .objective_reviewed_item_sources_ui import install_objective_reviewed_item_
 from .packaged_ui_policy import install_packaged_ui_policy
 from .profile_availability_ui import install_profile_availability_ui
 from .quest_interaction_guidance_ui import install_quest_interaction_guidance_ui
+from .quest_manual_progress_ui import install_quest_manual_progress_ui
 from .quest_progress_zone_context_ui import install_quest_progress_zone_context_ui
 from .runtime_mode_ui import install_runtime_mode_ui
 from .runtime_policy import install_runtime_policy
@@ -46,6 +47,7 @@ def install_application_layers() -> None:
     install_quest_progress_zone_context_ui()
     install_session_geography_ui()
     install_quest_interaction_guidance_ui()
+    install_quest_manual_progress_ui()
     install_activity_pathway_dismiss_ui()
     install_activity_clusters_ui()
     install_zone_opportunities_ui()
